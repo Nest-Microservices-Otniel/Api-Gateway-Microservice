38d86e177f68975f775bdf94a1cdf8add349df79
Otniel Lascano <otniel.lascano@ikualo.com>
2024-04-18 11:59:07 +0200

    lift all our services with docker

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,4 +1,5 @@
 import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+
 import { RpcException } from '@nestjs/microservices';
 
 @Catch(RpcException)
@@ -9,12 +10,21 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
     const rpcError = exception.getError();
 
+    if ( rpcError.toString().includes('Empty response') ) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1)
+      })
+    }
+
+
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
       'message' in rpcError
     ) {
-      const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+      const status = isNaN(+rpcError.status) ? 400 :+rpcError.status;
       return response.status(status).json(rpcError);
     }
 
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,22 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Inject,
-  ParseUUIDPipe,
-  Query,
-  Patch,
-} from '@nestjs/common';
-import { NATS_SERVICE} from 'src/config';
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
+
+
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
-import { catchError } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
+
   constructor(
     @Inject(NATS_SERVICE) private readonly client: ClientProxy,
   ) {}
@@ -27,48 +20,62 @@ export class OrdersController {
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.client.send('findAllOrders', orderPaginationDto);
+  async findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto)
+      )
+      return orders;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+  
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe ) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.client.send('findOneOrder', { id })
+      );
+
+      return order;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
-  @Get('id/:id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.client.send('findOneOrder', { id }).pipe(
-      catchError((error) => {
-        throw new RpcException(error);
-      }),
-    );
-  }
   @Get(':status')
-  findAllByStatus(
+  async findAllByStatus(
     @Param() statusDto: StatusDto,
     @Query() paginationDto: PaginationDto,
   ) {
-    return this.client
-      .send('findAllOrders', {
+    try {
+
+      return this.client.send('findAllOrders', {
         ...paginationDto,
         status: statusDto.status,
-      })
-      .pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      );
+      });
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
+
   @Patch(':id')
-  changeOrderStatus(
-    @Param('id', ParseUUIDPipe) id: string,
+  changeStatus(
+    @Param('id', ParseUUIDPipe ) id: string,
     @Body() statusDto: StatusDto,
   ) {
-    return this.client.send('changeOrderStatus', {
-      id,
-      status: statusDto.status,
-    })
-    .pipe(
-      catchError((error) => {
-        throw new RpcException(error);
-      }),
-    );
+    try {
+      return this.client.send('changeOrderStatus', { id, status: statusDto.status })
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
+
+
+
 }
